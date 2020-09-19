cd6fb62e6b89d77c596187794066624ff09ff833
Dhruv Sheth <67831664+dhruvsheth-ai@users.noreply.github.com>
2020-09-19 11:37:38 +0530

    Update dht11.py

--- dht11.py
+++ dht11.py
@@ -8,3 +8,19 @@ while True:
 
     print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
     time.sleep(1)
+if temperature < 8:
+    success = 0
+if (temperature > 8 and temperature < 15):
+    success = 1
+if (temperature > 15 and temperature < 21):
+    warning = 0
+if temperature > 21:
+    error = 0
+if humidity < 70:
+    success1 = 0
+if (humidity > 70 and humidity < 75):
+    success1 = 1
+if (temperature > 75 and temperature < 85):
+    warning1 = 0
+if temperature > 85:
+    error1 = 0    
