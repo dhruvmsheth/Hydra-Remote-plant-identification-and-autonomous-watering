b025f461c0997d2f5f02ac5e82b276471714927a
Dhruv Sheth <67831664+dhruvsheth-ai@users.noreply.github.com>
2020-09-19 13:46:19 +0530

    Create Datetime.py

---
+++ Datetime.py
@@ -0,0 +1,8 @@
+import datetime
+now = datetime.datetime.now()
+print ("Current date and time : ")
+#print (now.strftime("%Y-%m-%d %H:%M:%S"))
+x = int(now.strftime("%H"))
+print(x)
+if x == 1:
+  a == int(temperature)
